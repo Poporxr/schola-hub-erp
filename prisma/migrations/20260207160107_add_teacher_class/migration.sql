a73cffaa8e9ca5e1f696c7297a8e6c424cead2db
Terngu Emanuel <poporxr@gmail.com>
2026-02-22 16:48:06 +0100

    reverted my code to four commits back

--- prisma/migrations/20260207160107_add_teacher_class/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Teacher" ADD COLUMN     "classId" TEXT;
-
--- CreateIndex
-CREATE INDEX "Teacher_classId_idx" ON "Teacher"("classId");
-
--- AddForeignKey
-ALTER TABLE "Teacher" ADD CONSTRAINT "Teacher_classId_fkey" FOREIGN KEY ("classId") REFERENCES "Class"("id") ON DELETE SET NULL ON UPDATE CASCADE;
